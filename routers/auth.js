1907774840d33f64853fc3faa560fad8f4138b28
4ppe <alper0524@gmail.com>
2020-12-19 06:24:43 +0300

    Database connection with mongoose

--- routers/auth.js
+++ routers/auth.js
@@ -4,7 +4,7 @@ const express = require('express');
 const {register} = require('../controllers/auth')
 const router = express.Router();
 
-router.get("/register",register);
+router.post("/register",register);
 
 
 module.exports = router;
