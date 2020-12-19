3d6c547ee749eba98d451ce67d866e6791740dc9
4ppe <alper0524@gmail.com>
2020-12-19 04:01:55 +0300

    Environment Variables Added

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv')
+
+// Environment Variables
+dotenv.config({
+    path : "./config/env/config.env"
+});
 
 const app = express();
+const PORT = process.env.PORT;
 
-const PORT = 5000 || process.env.PORT;
 
 app.get("/",(req,res) => {
 
     res.send("my API");
 });
 
+
 app.listen(PORT, () => {
-    console.log(`App started on ${PORT}`);
+    console.log(`App started on ${PORT} : ${process.env.NODE_ENV}`);
 });
