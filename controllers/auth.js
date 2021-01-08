c52d55919c3f3a062efdfa4c1bcc22471c186faf
4ppe <alper0524@gmail.com>
2021-01-08 06:55:49 +0300

    register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,22 @@
-const register = (req,res,next) => {
+const User = require("../models/User");
+
+const register = async (req,res,next) => {
+    // TODO: POST DATA
+    const name = "Alpzazaer"
+    const email = "alper1@alper.com"
+    const password = "123123123"
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
 
     res
     .status(200)
     .json({
-        success: true
+        success: true,
+        data: user
     })
 }
 
