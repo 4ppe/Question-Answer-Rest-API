e7db2bc9efc60cddb51263045d78564bd1ba3510
4ppe <alper0524@gmail.com>
2021-02-04 17:17:19 +0300

    add new question

--- controllers/question.js
+++ controllers/question.js
@@ -1,14 +1,19 @@
-const getAllQuestions = (req,res,next) => {
+const Question = require("../models/Question");
+const CustomError = require("../helpers/error/CustomError");
+const asyncHandler = require("express-async-handler");
 
-    res
-    .status(200)
-    .json({
-        success: true
+const askNewQuestion = asyncHandler(async (req, res, next) => {
+    const information = req.body;
+    const question = await Question.create({
+        ...information,
+        user: req.user.id
     });
-
-};
-
+    res.status(200).json({
+        success: true,
+        data: question
+    });
+});
 
 module.exports = {
-    getAllQuestions
+    askNewQuestion
 };
