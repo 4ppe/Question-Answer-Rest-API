8c57625a9bb8776fdea9601948314d90794751a0
4ppe <alper0524@gmail.com>
2021-02-11 04:37:48 +0300

    detailed information of the questions

--- controllers/question.js
+++ controllers/question.js
@@ -17,12 +17,20 @@ const askNewQuestion = asyncHandler(async (req, res, next) => {
 
 const getAllQuestions = asyncHandler(async (req, res, next) => {
     let query = Question.find();
+    const populate = true;
+    const populateObject = {
+        path: "user",
+        select: "name profile_image"
+    };
     if (req.query.search) {
         const searchObject = {};
         const regex = new RegExp(req.query.search, "i");
         searchObject["title"] = regex;
 
         query = query.where(searchObject)
+    };
+    if (populate) {
+        query = query.populate(populateObject)
     }
     const questions = await query;
     res.status(200).json({
